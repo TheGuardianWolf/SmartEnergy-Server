1521e0211171c54a3e6f8d3a4383f440c831b3e1
Aeon Firebrand <lichking21st@windowslive.com>
2016-07-23 05:15:17 +1200

    MVC Models set up for entity framework
    Updated NuGet packages, testing deployment via github.

---
+++ SmartEnergy-Server/Models/Data.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartEnergy_Server.Models
+{
+    public class Data
+    {
+        public int Id { get; set; }
+        public DateTime Time { get; set; }
+        public int Power { get; set; }
+
+        [JsonIgnore]
+        public virtual Devices Device { get; set; }
+
+        [JsonIgnore]
+        public virtual Users User { get; set; }
+    }
+}
---
+++ SmartEnergy-Server/Models/Users.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartEnergy_Server.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+    }
+}
